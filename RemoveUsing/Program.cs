c44eb8f9a41a5257ce49b26912f7c8f9aac56d6d
BetoBarros07 <betobarros07@gmail.com>
2018-01-16 11:27:17 -0200

    First commit.

---
+++ RemoveUsing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.File;
+
+namespace RemoveUsing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Starting to remove all regions in your C# project.");
+            Write("Path of solution: ");
+            var path = ReadLine();
+            string[] files = GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                var lines = ReadAllLines(file);
+                var stringBuilder = new StringBuilder();
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    var line = lines[j];
+                    if (line.Contains("#region") || line.Contains("#endregion"))
+                        continue;
+                    stringBuilder.AppendLine(line);
+                }
+                WriteAllText(file, stringBuilder.ToString());
+                WriteLine($"DONE: {file}");
+            }
+            Read();
+        }
+    }
+}
